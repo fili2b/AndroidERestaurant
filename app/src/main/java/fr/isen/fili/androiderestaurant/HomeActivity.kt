8f8f20611ec7c8a5ce12f3e638765faa8372bd06
fili2b <therese.fili@isen.yncrea.fr>
2021-01-25 11:39:25 +0100

    Ajout du toast

--- app/src/main/java/fr/isen/fili/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/fili/androiderestaurant/HomeActivity.kt
@@ -1,11 +1,30 @@
 package fr.isen.fili.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import fr.isen.fili.androiderestaurant.databinding.ActivityHomeBinding
+
+private lateinit var binding: ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.entreeButton.setOnClickListener{
+            val intent = Intent(this, CategoryActivity::class.java)
+            val toast = Toast.makeText(applicationContext, "Entrée sélectionnée", Toast.LENGTH_SHORT)
+            toast.show()
+            startActivity(intent)
+        }
     }
+
+
 }
