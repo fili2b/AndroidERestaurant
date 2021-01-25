ca4ad47bce83d0e9be2560d0954fc4aab5d1b0a3
fili2b <therese.fili@isen.yncrea.fr>
2021-01-25 17:10:41 +0100

    Ajout du RecyclerView et de l'Adapter

--- app/src/main/java/fr/isen/fili/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/fili/androiderestaurant/CategoryActivity.kt
@@ -2,9 +2,12 @@ package fr.isen.fili.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import fr.isen.fili.androiderestaurant.databinding.ActivityCategoryBinding
 
 private lateinit var binding: ActivityCategoryBinding
+private lateinit var linearLayoutManager: LinearLayoutManager
+
 
 class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +17,8 @@ class CategoryActivity : AppCompatActivity() {
 
         binding.categoryTitle.text = intent.getStringExtra(HomeActivity.CATEGORY)
 
+        binding.listCategory.layoutManager = LinearLayoutManager(this)
+        //val userName = ressources.geyStringArray(R.array.planets_array).toList()
+        binding.listCategory.adapter = CategoryListAdapter(listOf("julien","pierre","paul"))
     }
 }
---
+++ app/src/main/java/fr/isen/fili/androiderestaurant/CategoryListAdapter.kt
@@ -0,0 +1,28 @@
+package fr.isen.fili.androiderestaurant
+import android.view.LayoutInflater
+import android.view.ViewGroup;
+import androidx.recyclerview.widget.RecyclerView;
+import fr.isen.fili.androiderestaurant.databinding.CategoryCellBinding
+
+class CategoryListAdapter(val categories: List<String>): RecyclerView.Adapter<CategoryListAdapter.CategoryHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CategoryHolder {
+        val itemBinding = CategoryCellBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return CategoryHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: CategoryHolder, position: Int) {
+        holder.title.text = categories[position]
+    }
+
+    override fun getItemCount(): Int {
+        return categories.size
+    }
+
+    class CategoryHolder(binding: CategoryCellBinding): RecyclerView.ViewHolder(binding.root){
+        val title = binding.dishName
+    }
+}
+
