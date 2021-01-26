0fabcecc2a8effd97810e372da5b438fab5750e3
fili2b <therese.fili@isen.yncrea.fr>
2021-01-26 14:02:46 +0100

    Ajout de la classe data Ingredient et Price, modification de la classe Dish

--- app/src/main/java/model/Dish.kt
+++ app/src/main/java/model/Dish.kt
@@ -5,9 +5,20 @@ import java.io.Serializable
 
 data class Dish(
     @SerializedName("name_fr") val title: String,
-    //@SerializedName("ingredients") val ingredients: List<Ingredient>,
-    @SerializedName("images") private val pictures: List<String>
-    //@SerializedName("prices") private val prices: List<Price>
+    @SerializedName("ingredients") val ingredients: List<Ingredient>,
+    @SerializedName("images") private val pictures: List<String>,
+    @SerializedName("prices") private val prices: List<Price>
 ): Serializable {
-    //fun getPrice()
+    fun getPrice() = prices[0].price + "€"
+    fun getFormattedPrice() = prices[0].price + "€"
+    fun getFirstPicture() = if (pictures.isNotEmpty() && pictures[0].isNotEmpty()) {
+        pictures[0]
+    } else {
+        null
+    }
+    fun getAllPictures() = if(pictures.isNotEmpty() && pictures.any {it.isNotEmpty()}) {
+        pictures.filter { it.isNotEmpty() }
+    }else {
+        null
+    }
 }
---
+++ app/src/main/java/model/Price.kt
@@ -0,0 +1,8 @@
+package model
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class Price(
+    @SerializedName("price") val price: String
+): Serializable
