6cbe0d53a45cea33842f7dc275ec6f552965116e
fili2b <therese.fili@isen.yncrea.fr>
2021-02-02 22:38:41 +0100

    Amelioration du design : fix de bug d'affichage sur la page accueil, panier, login, register

---
+++ app/src/main/java/fr/isen/fili/androiderestaurant/Order.kt
@@ -0,0 +1,10 @@
+package fr.isen.fili.androiderestaurant
+import com.google.gson.annotations.SerializedName
+import java.io.File
+import java.io.Serializable
+
+data class Order (
+    @SerializedName("id_shop") val id_shop : String,
+    @SerializedName("id_user") val id_user : String,
+    @SerializedName("basket") val basket: File
+) : Serializable
